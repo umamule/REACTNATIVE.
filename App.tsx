fdbf6a514dc5c05b9714d56e2f31b57332e841d7
umamule <umule33@gmail.com>
2025-01-03 13:28:48 +0530

    hello world app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View,SafeAreaView,StyleSheet } from 'react-native'
 
 export default function App(){
   
     return (
-      <View>
-        <Text> textInComponent </Text>
+
+      <View style={styles.container}>
+        <Text> Hello world!! </Text>
       </View>
     )
   }
+  const styles=StyleSheet.create(
+    {
+      container:{
+        //backgroundColor:'#000000',
+        color:'#FFFFFF',
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+      }
+    }
+  )
 
 
 
